d8d44ca78b4c968412109d35ab2afbea021e59a5
jaeykimusa <85996421+jaeykimusa@users.noreply.github.com>
2025-12-02 03:51:20 -0500

    [Implementation] Updated URDF filename to go2_EEs.urdf

--- src/robot_description/go2_description/go2_pinocchio.py
+++ src/robot_description/go2_description/go2_pinocchio.py
@@ -11,7 +11,7 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Build the path relative to this file
-urdf_filename = os.path.join(current_dir, "go2", "go2.urdf")
+urdf_filename = os.path.join(current_dir, "go2", "go2_EEs.urdf")
 
 joint_model = pin.JointModelComposite(2)
 joint_model.addJoint(pin.JointModelTranslation())
